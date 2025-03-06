2301036f96b3022fcb6ea806565d852e3eb68255
Nzile22 <diananduku00@gmail.com>
2025-03-06 15:44:23 +0300

    initial commit

---
+++ app/migrations/versions/65e22c4bf4c3_initial_migration.py
@@ -0,0 +1,24 @@
+"""Initial Migration
+
+Revision ID: 65e22c4bf4c3
+Revises: 
+Create Date: 2025-03-06 12:30:31.034020
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '65e22c4bf4c3'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
